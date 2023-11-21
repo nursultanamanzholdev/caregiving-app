3bf2410779b712ef058316b4637cc023512c8d06
nursultanamanzholdev <148472697+nursultanamanzholdev@users.noreply.github.com>
2023-11-21 21:28:49 +0600

    Update urls.py

--- ass2/urls.py
+++ ass2/urls.py
@@ -20,7 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('caregiving_app.urls')),
+    path('https://caregiving-app.onrender.com/', include('caregiving_app.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
